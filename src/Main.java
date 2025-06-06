02ecc88e590dbfb73348b4f2b836f4c6d0d3c3bd
Sangho Kim <ksangho0823@gmail.com>
2025-06-06 16:23:50 -0400

    init commit

---
+++ src/Main.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    // Student Class
+    @SuppressWarnings("all")
+    public static class Student {
+        private final String name;
+        private final String subject;
+        private final double grade;
+
+        Student(String name, String subject, double grade) {
+            this.name = name;
+            this.subject = subject;
+            this.grade = grade;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getSubject() {
+            return subject;
+        }
+
+        public double getGrade() {
+            return grade;
+        }
+    }
+
+    // want to return list of students' subject grade medians as k : v pair, where k = subject & v = median value
+    public static Collector<Student, ?, Map<String, Double>> getSubjectMedians() {
+        return Collectors.groupingBy(
+                Student::getSubject,
+                Collectors.collectingAndThen(
+                    Collectors.mapping(Student::getGrade, Collectors.toList()),
+                    subjectStudents -> {
+                        System.out.println("TEST 1 === " + subjectStudents);
+                        List<Double> grades = subjectStudents.stream()
+                                .sorted()
+                                .toList();
+                        int size = subjectStudents.size();
+                        if(size % 2 == 0) {
+                            return (grades.get(grades.size() / 2 - 1) + grades.get(grades.size() / 2)) / 2;
+                        }
+                        else {
+                            return grades.get(grades.size() / 2);
+                        }
+                    }
+        ));
+    }
+
+    public static void main( String[] args ) {
+
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Test " + 1, "Physics", 97.0));
+        students.add(new Student("Test " + 2, "Physics", 77.0));
+        students.add(new Student("Test " + 3, "Physics", 87.0));
+        students.add(new Student("Test " + 4, "Physics", 47.0));
+        students.add(new Student("Test " + 5, "Physics", 83.0));
+        students.add(new Student("Test " + 6, "Biology", 63.0));
+        students.add(new Student("Test " + 7, "Biology", 73.0));
+        students.add(new Student("Test " + 8, "Biology", 93.0));
+        students.add(new Student("Test " + 9, "Chemistry", 91.0));
+        students.add(new Student("Test " + 10, "Chemistry", 81.0));
+
+        Map<String, Double> subjectMedians = students.stream().collect(getSubjectMedians());
+        System.out.println("subjectMedians = " + subjectMedians);
+    }
+}
