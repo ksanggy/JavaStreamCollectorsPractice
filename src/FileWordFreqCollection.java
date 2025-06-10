61d6c957b02348882e2b0e2107a49619f98a773e
Sangho Kim <ksangho0823@gmail.com>
2025-06-09 21:35:36 -0400

    added comment on method

--- src/FileWordFreqCollection.java
+++ src/FileWordFreqCollection.java
@@ -9,6 +9,19 @@ import java.util.Arrays;
 
 public class FileWordFreqCollection {
 
+    /**
+     * Creates a collector that counts the frequency of each word in a stream.
+     * <a/>
+     * Function.identity() is used as the classifier function in groupingBy.
+     * It's equivalent to writing (x) -> x, meaning "use the element itself as the key".
+     * For example, if we have words ["this", "is", "this", "test"],
+     * it will group them by the word itself, resulting in:
+     * {"this": 2, "is": 1, "test": 1}
+     * <a/>
+     * @return A collector that produces a Map<String, Long> where:
+     *         - Key: the word
+     *         - Value: the count of occurrences
+     */
     public static Collector<String, ?, Map<String, Long>> wordFreqCollector() {
         return Collectors.groupingBy(
                 Function.identity(),
